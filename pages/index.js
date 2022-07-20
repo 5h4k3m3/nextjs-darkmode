389d6c2de69eb3b0f4989a77c1a09a35342820e2
SalmonFillet <sy4.l3ucas@gmail.com>
2022-07-20 21:59:44 +0900

    ✨ impl page and style

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,34 @@
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="container">
+        <nav>
+          <div className="title">My Portfolio</div>
+          <div>
+            <form action="#">
+              <label className="switch">
+                <input type="checkbox" />
+                <span className="slider"></span>
+              </label>
+            </form>
+          </div>
+        </nav>
+        <section>
+          <div className="content">
+            <h1>Salmon Portfolio</h1>
+            <h3>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam,
+              voluptatum?
+            </h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Asperiores molestiae obcaecati error odit eaque natus nihil
+              deleniti quo voluptatibus. Vitae.
+            </p>
+            <button className="primary-btn">Contact</button>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
 }
